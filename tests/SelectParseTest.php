71bcbb2255e40182d292788d3671762cbca9ea6a
Matt Brown <github@muglug.com>
2021-01-16 11:15:26 -0500

    Add test for coerced date

--- tests/SelectParseTest.php
+++ tests/SelectParseTest.php
@@ -270,7 +270,8 @@ class SelectParseTest extends \PHPUnit\Framework\TestCase
     {
         $query = 'SELECT DATE_SUB(\'2020-03-01 12:00:00\', INTERVAL 1 HOUR) as `a`,
                         DATE_ADD(\'2020-03-01 12:00:00\', INTERVAL 1 HOUR) as `b`,
-                        DATEDIFF(\'2017-01-01\', \'2016-12-24\') AS `c`;';
+                        DATEDIFF(\'2017-01-01\', \'2016-12-24\') AS `c`,
+                        DATE(DATE_SUB(\'2020-03-01 12:00:00\', INTERVAL 1 HOUR)) as `d`';
 
         $select_query = \Vimeo\MysqlEngine\Parser\SQLParser::parse($query);
 
@@ -279,7 +280,7 @@ class SelectParseTest extends \PHPUnit\Framework\TestCase
         $conn = new \Vimeo\MysqlEngine\FakePdo('mysql:foo');
 
         $this->assertSame(
-            [['a' => '2020-03-01 11:00:00', 'b' => '2020-03-01 13:00:00', 'c' => 8]],
+            [['a' => '2020-03-01 11:00:00', 'b' => '2020-03-01 13:00:00', 'c' => 8, 'd' => '2020-03-01']],
             \Vimeo\MysqlEngine\Processor\SelectProcessor::process(
                 $conn,
                 new \Vimeo\MysqlEngine\Processor\Scope(),
